8953d80fa6328cf3751a79fcd51af21d71b0cc55
oogabooga <twgosaas@gmail.com>
2012-02-24 18:26:17 +0530

    Creating a simple Rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sherlock::Application.config.secret_token = 'd6a913fa47099ac2f64e9f85fdd1f9e8923ff77c2125b4382877c838c6fdc03fcffc56f17f0045222fe9087003a1530b85476d0885900dfad190e029b04d380e'
